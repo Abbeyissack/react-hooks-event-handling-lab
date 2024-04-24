c979e4af0f956743d179040e89c6fa7274d35e0d
Uwes Ahabab <uwesahabab@gmail.com>
2024-04-24 19:39:22 +0300

    Event Handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  function inputHandler() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={inputHandler}
+        placeholder="ENTER PASSWORD"
+      />
+    </div>
+  );
 }
 
 export default Keypad;
